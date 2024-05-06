9c3a1f3502b90a85526eb114db221f2ba3bb5fa0
tsarkoff <tsarkov.vladimir@gmail.com>
2024-05-07 00:11:03 +0300

    Stub and tests for add

--- src/main/java/PhoneBook.java
+++ src/main/java/PhoneBook.java
@@ -1,5 +1,22 @@
 
 // Этап-1 = создан пустой класс PhoneBook
+// Этап-3 = добавлены параметры в метод add() + книга, хранящая уникальную мапу контактов с несколькими телефонными номерами + vметод получения количества контактов
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 public class PhoneBook {
+    private int contactsNumber = -1;
+
+    public int getContactsNumber() {
+        return contactsNumber;
+    }
+
+    public Map<String, List<String>> contacts = new HashMap<>();
+
+    public int add(String name, String phoneNumber) {
+        contactsNumber++;
+        return 0;
+    }
 }
--- src/test/java/PhoneBookTest.java
+++ src/test/java/PhoneBookTest.java
@@ -1,5 +1,32 @@
 
 // Этап-2 = создан пустой класс PhoneBookTest
+// Этап-3+ = созданы тесты для добавления имени/номера
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 public class PhoneBookTest {
+    private static PhoneBook phoneBook = null;
+
+    @BeforeAll
+    static void setUp() {
+        phoneBook = new PhoneBook();
+    }
+
+    @ParameterizedTest
+    @CsvSource(value = {
+            "Alex, 89967467999",
+            "Sophia, 79967467999",
+            "Jan, +79967467999",
+            "Alex, +19967467999",
+            "Jan, +29967467999",
+            "Alex, false"
+    })
+
+    public void add(String name, String phoneNumber) {
+        int contactsNumber = phoneBook.add(name, phoneNumber);
+        Assertions.assertEquals(contactsNumber, phoneBook.getContactsNumber());
+    }
 }
